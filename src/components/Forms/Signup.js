6bcea693f57e5dd82cf6f5b048e8e956929e7d7f
Kaloyan Daskalov <kdaskalov78@gmail.com>
2021-01-09 15:37:59 +0200

    Signin form modifications

--- src/components/Forms/Signup.js
+++ src/components/Forms/Signup.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Forms.module.css';
 import Button from '../UI/Button/Button';
 import Input from '../UI/Input/Input';
 
-const signup = () => (
-    <div className={classes.Container}>
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirm, setConfirm] = useState('');
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log(email, password, confirm);
+    };
+
+    return (<div className={classes.Container}>
         <header className={classes.Header}>
             <h2 className={classes.Heading}>Sign Up</h2>
             <p className={classes.HeaderMessage}>
                 Already a member? <a href="#" className={classes.HeaderLink}>Log In</a>
             </p>
-            <p className={classes.Message}>Password must be 4 to 10 characters</p>
+            <p className={classes.Message}>Error Message</p>
         </header>
-        <form className={classes.Form}>
-            <input className={classes.Input} type="text" placeholder="Email" />
-            <input className={classes.Input} type="password" placeholder="Password" />
-            <input className={classes.Input} type="password" placeholder="Confirm Password" />
+        <form className={classes.Form} onSubmit={submitHandler} >
+            <Input getValue={(e) => setEmail(e.target.value)} showError={false} attributes={{ type: 'text', placeholder: 'Email' }} />
+            <Input getValue={(e) => setPassword(e.target.value)} showError attributes={{ type: 'password', placeholder: 'Password' }} />
+            <Input getValue={(e) => setConfirm(e.target.value)} showError attributes={{ type: 'password', placeholder: 'Confirm Password' }} />
+            <p className={classes.Instructions}>Password must be at least 6 characters</p>
             <Button>Sign Up</Button>
         </form>
-    </div>
-);
+    </div>);
+};
 
-export default signup;
+export default Signup;
